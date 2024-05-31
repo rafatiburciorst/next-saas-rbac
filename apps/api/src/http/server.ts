deca378f6fb4a9b31cc8e25cb3b792ecfe195727
Rafael Tiburcio <rafael.tiburcio@live.com>
2024-05-31 10:50:49 -0300

    steps

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,19 +1,49 @@
 import { fastify } from 'fastify'
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import fastifyJwt from '@fastify/jwt'
 import {
-  //   jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
+import { getProfile } from './routes/auth/get-profile'
+import { errorHandler } from './error-handler'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
+app.setErrorHandler(errorHandler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Fullstack saas App with multi-tenant & RBAC',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+  secret: 'secret',
+})
+
 app.register(fastifyCors)
 app.register(createAccount)
+app.register(authenticateWithPassword)
+app.register(getProfile)
 
 app
   .listen({
